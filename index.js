a78eacbca8b07003ae0364c23848c647717ff5fa
adityaCODING-a18 <adityar18j@gmail.com>
2025-04-01 18:47:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -74,6 +74,7 @@ io.on("connection", (socket) => {
 });
 
 // Start server
-server.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+server.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on port ${PORT}`);
 });
+
